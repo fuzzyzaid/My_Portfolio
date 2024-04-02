2d343703efba64a390f210470a8d26985f81327e
Zaid Alam <zaidalam0731@gmail.com>
2024-04-02 02:59:50 -0400

    Updated Header

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,17 +1,21 @@
 import React from 'react'
+import styles from './About.module.css'
 
 function About() {
   return (
-   <>
-     <h2>About Me</h2>
-     <p>
-     Experienced Full-Stack Developer with 3+ years in crafting responsive websites. Skilled in analyzing
-     project requirements, adapting to new technologies, and integrating frontend and backend for cohesive
-     solutions. Passionate about staying updated on industry trends and implementing best practices for
-     efficient development.       
-     </p>
-   </>
-  )
+    <div className={`about-container vh-100 d-flex justify-content-center align-items-center bg-white ${styles.aboutContainer}`}>
+      <div className="text-center">
+        <h2 className="display-4 mb-4">About Me</h2>
+        <p className="text-center">
+          Welcome to my portfolio! I'm Zaid Alam, a seasoned Full-Stack Developer with over three years of experience in crafting responsive websites. 
+          I thrive on analyzing project requirements, seamlessly integrating front and backend technologies, and delivering cohesive solutions that exceed expectations. 
+          My passion lies in staying at the forefront of industry trends, ensuring that I implement the best practices for efficient development. 
+          Join me on this journey as we explore the dynamic world of software development together.
+        </p>
+      </div>
+    </div>
+  );
 }
+
 
 export default About
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,48 @@
-import React from 'react';
-import styles from './Header.module.css'
+// Header.jsx
+
+import React, { useState } from 'react';
+import { FaTimes } from 'react-icons/fa'; // Import close icon from react-icons
+import styles from './Header.module.css';
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <>
-      <header className="bg-dark text-light py-3">
+    <div className={`vh-100 ${menuOpen ? `${styles.menuOpen}`: ''}`}>
+      <nav className="navbar navbar-expand-lg navbar-light mt-4">
         <div className="container">
-          <h1 className="display-5 mb-3">Zaid Alam</h1>
-          <p className="lead mb-4">Software Developer</p>
-          <div>
-            <a href="mailto:zaidalam0731@gmail.com" className={`text-light ${styles.contact}`} target="_blank" rel="noopener noreferrer">Mail</a>
-            <a href="https://www.linkedin.com/in/zaid-alam/" className={`text-light ${styles.contact}`} target="_blank" rel="noopener noreferrer">LinkedIn</a>
-            <a href="https://github.com/fuzzyzaid" className={`text-light ${styles.contact}`} target="_blank" rel="noopener noreferrer">Github</a>
-            <a href="tel:+12267538192" className={`text-light ${styles.contact}`} target="_blank" rel="noopener noreferrer">+1 (226) 753-8192</a>
+          <button className="navbar-toggler ml-auto" type="button" onClick={toggleMenu}>
+            {menuOpen ? <FaTimes /> : <span className="navbar-toggler-icon"></span>}
+          </button>
+          <div className={`collapse navbar-collapse ${menuOpen ? 'show' : ''}`}>
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link" href="mailto:zaidalam0731@gmail.com"><i className={`${styles.mail}bi bi-envelope h1 mr-4`}></i></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="https://www.linkedin.com/in/zaid-alam/"><i className={`${styles.linkedIn} bi bi-linkedin h1  mr-4`}></i></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="https://github.com/fuzzyzaid"><i className={`${styles.github} bi bi-github h1  mr-4`}></i></a>               
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="tel:+12267538192"><i className={`${styles.phone} bi bi-phone h1  mr-4`}></i></a>
+              </li>
+            </ul>
           </div>
         </div>
-      </header>
-    </>
+      </nav>
+      <div className={`${styles.headerInfo}`}>
+        <h1 className={`${styles.name}`}>Zaid Alam</h1> {/* Larger font */}
+        <p className={`${styles.title} mt-2`}>Full Stack Developer</p>
+        <p className={`${styles.intro} mt-2`}>Passionate about building scalable and user-friendly web applications.</p>
+        <img src="zaid.jpg" class= {`${styles.profile} rounded-4`} alt="Zaid Alam" />
+      </div>
+    </div>
   );
 }
 
